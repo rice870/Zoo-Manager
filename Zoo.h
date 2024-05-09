209b085b36510dc47d8184efd88767c0a06d7f06
Cooper William Melville <a1889786@embg13w035.ad.adelaide.edu.au>
2024-05-09 13:31:27 +0930

    Defined functions in the Zoo class

--- Zoo.h
+++ Zoo.h
@@ -2,6 +2,7 @@
 #define ZOO_H
 
 #import <string>
+#import <vector>
 #import <iostream>
 #include "Animal.h"
 #include "Person.h"
@@ -14,16 +15,58 @@ class Zoo {
 
     private:
         string name;
-        string
+        string location;
 
-        Animal* animals;
+        bool openStatus;
+        vector<Animal*> animals;
+        int animalCapacity;
+        int currentAnimals;
+
 
     public:
-        string get_name();
-        Animal* get_animals();
-        void set_name();
-        void addAnimal();
-        
+        Zoo() {
+            name = "";
+            openStatus = false;
+            currentAnimals = 0;
+        }
+        Zoo(string _name, bool _openStatus, int _animalCapacity) {
+            name = _name;
+            openStatus = _openStatus;
+            animalCapacity = _animalCapacity;
+            currentAnimals = 0;
+
+        }
+        string get_name() {
+            return name;
+        }
+        vector get_animals() {
+            return animals;
+        }
+        void set_name(string _name) {
+            name = _name;
+        }
+        bool get_openStatus() {
+            return openStatus;
+        }
+        void set_openStatus(bool _openStatus) {
+            openStatus = _openStatus;
+        }
+        void addAnimal(Animal* _animal) {
+            animals.push_back(_animal);
+        }
+        void openZoo() {
+            openStatus = true;
+        }
+        void clozeZoo() {
+            openStatus = false;
+        }
+        void set_animalCapacity(int _animalCapacity) {
+            animalCapacity = _animalCapacity;
+        }
+
+
+
+
 
 
 
