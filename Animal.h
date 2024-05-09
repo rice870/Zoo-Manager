772322918e9b2a459549336c742f0ffc238bd73c
Jared <ricjm008@mymail.unisa.edu.au>
2024-05-09 12:29:17 +0930

    animalh

---
+++ Animal.h
@@ -0,0 +1,17 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Animal{
+    protected:
+        int health;
+        int happiness;
+        int hunger;
+        string species;
+
+};
+
+#endif
