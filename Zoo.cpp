41e1c7ad3e009e4f6a66af9ef5bfa198b0e2d223
Cooper Melville <coopmel043@gmail.com>
2024-05-17 13:08:10 +0930

    Included all header files into this .h

--- Zoo.cpp
+++ Zoo.cpp
@@ -2,10 +2,15 @@
 #include <vector>
 #include <iostream>
 #include "Animal.h"
-#include "Person.h"
+#include "Panda.h"
+#include "Kangaroo.h"
+#include "Elephant.h"
+#include "Tortoise.h"
+#include "Kiwi.h"
 #include "Enclosure.h"
-#include "Zoo.h"
-
+#include "Facility.h"
+#include "Person.h"
+#include "Visitor.h"
 Zoo::Zoo() {
     name = "";
     openStatus = false;
