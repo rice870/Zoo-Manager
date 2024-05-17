29459b3e0cb454023947df473936c4564f1f4b2b
Cooper Melville <coopmel043@gmail.com>
2024-05-17 13:07:47 +0930

    Included all header files into this .h

--- Zoo.h
+++ Zoo.h
@@ -6,8 +6,15 @@
 #include <vector>
 
 #include "Animal.h"
-#include "Person.h"
+#include "Panda.h"
+#include "Kangaroo.h"
+#include "Elephant.h"
+#include "Tortoise.h"
+#include "Kiwi.h"
 #include "Enclosure.h"
+#include "Facility.h"
+#include "Person.h"
+#include "Visitor.h"
 
 using namespace std;
 
